72194c675095ce05458fc642ed635dad956fafa7
SajidBhatti786 <sbhattti1212@gmail.com>
2022-10-31 19:11:43 +0500

    This file is customizing your linux like macBook... Read ReadMe file complete instructions

---
+++ gnome.sh
@@ -0,0 +1,8 @@
+#Author Sajid ali
+#This script will help you to install customization tool for ubuntu
+#!/bin/bash
+sudo apt-get install gnome-tweaks
+sudo apt-get install gnome-shell-extensions
+sudo apt-get install chrome-gnome-shell
+sudo apt-get update
+
